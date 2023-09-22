6d9c7c838e22c444f36b809e85f7527e1c527dbf
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-09-22 09:13:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-has_enough_units = game.ask("Do you have enough credits?", "OK = True, Cancel = False")
-has_met_requirements = game.ask("Have you met the requirements", "OK = True, Cancel = False")
 
-if has_enough_units and has_met_requirements:
-    adventure.add_to_textlog("You can graduate.")
-else:
-    adventure.add_to_textlog("You cannot graduate.")
